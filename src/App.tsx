bdf28a83f8b409414698ab5520f212d6cda1cc3a
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2023-01-01 19:10:24 +0200

    Add queryClientProvider and reactQueryDevtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-function App() {
-  return <div></div>;
-}
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-export default App;
+const queryClient = new QueryClient();
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1 className="text-3xl text-center pt-5">Infinite Scroll</h1>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
